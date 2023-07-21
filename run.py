3b3f295ba2c2fdcccf56dbbabe3ae4652a77970f
Jan Novopacky <jan.novopacky@gmail.com>
2023-07-21 17:12:54 +0200

    final tuoch

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ async def register(db: Session = Depends(get_db)):
     new_client_id = secrets.token_urlsafe(16)
     db.add(User(client_id=new_client_id))
     db.commit()
-    return {"client_id": new_client_id}
+    return JSONResponse(status_code=202, content={"message": "Cliend_id created successfully.","client_id": new_client_id})
 
 
 @app.post("/webhook/{client_id}", response_model=None)
@@ -66,7 +66,7 @@ async def register_webhook(
         raise HTTPException(status_code=400, detail="Invalid client id")
     user.webhook_url = webhook_url
     db.commit()
-    return {"message": "Webhook registered successfully"}
+    return JSONResponse(status_code=202, content={"message": "Webhook registered successfully", "webhook_url": str(user.webhook_url)})
 
 
 @app.get("/scrape/{base_url:path}", response_model=None)
@@ -74,19 +74,19 @@ async def scrape(
     base_url: str, client_id: str, background_tasks: BackgroundTasks, db: Session = Depends(get_db)
 ):
     user = db.query(User).filter(User.client_id == client_id).first()
-    logger.warning(f"Sarted scraping {base_url}")
+    logger.warning(f"Started scraping {base_url}")
     if user is None:
         raise HTTPException(status_code=400, detail="Invalid client id")
 
     # Add the scraping task to the background tasks queue
     background_tasks.add_task(scrape_and_notify, db, client_id, base_url)
 
-    return JSONResponse(status_code=202, content={"message": "Scrape job started"})
+    return JSONResponse(status_code=202, content={"message": "Scrape job started", "base_url": str(base_url), "webhook_url": str(user.webhook_url)})
 
 
 
 async def scrape_and_notify(db: Session, client_id: str, base_url: str):
-    records = await scrape_website_async(f"https://{base_url}/")
+    records = await scrape_website_async(base_url=f"https://{base_url}/", concurrent_tasks=10)
     logger.warning(f"Finished scraping {base_url}")
     await notify_webhook(db, client_id, {"records": [record.__dict__ for record in records]})
 
--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
-import logging
 import aiohttp
 import asyncio
-
+import time
 
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -10,18 +9,19 @@ from urllib.robotparser import RobotFileParser
 from models import UrlData
 from logging_conf import logger
 
+
 async def fetch(session, url):
     try:
         async with session.get(url) as response:
             return await response.text()
     except UnicodeDecodeError as error:
-        logger.error(f'Error fetching URL {url}: {error}')
-        return ''
+        logger.info(f"Error fetching URL {url}: {error}")
+        return ""
 
 
 async def fetch_robots(session, base_url):
     robots = RobotFileParser()
-    robots_url = urljoin(base_url, '/robots.txt')
+    robots_url = urljoin(base_url, "/robots.txt")
     content = await fetch(session, robots_url)
     if content:
         robots.parse(content.splitlines())
@@ -33,61 +33,94 @@ async def parse(session, base_url, url, depth=0, records_len=0):
         return None, []
 
     body = await fetch(session, url)
-    soup = BeautifulSoup(body, 'html.parser')
+    soup = BeautifulSoup(body, "html.parser")
 
-    titles = {f"h{i}": (soup.find(f"h{i}").get_text(strip=True)
-                        if soup.find(f"h{i}") else None)
-              for i in range(1, 6)}
-    paragraphs = ' '.join(p.get_text(' ', strip=True)
-                          for p in soup.find_all('p'))
+    titles = {
+        f"h{i}": (
+            soup.find(f"h{i}").get_text(strip=True) if soup.find(f"h{i}") else None
+        )
+        for i in range(1, 6)
+    }
+    paragraphs = " ".join(p.get_text(" ", strip=True).replace('\u200d', ' ') for p in soup.find_all("p"))
 
     urls = []
     if depth < 2:
-        urls = [urljoin(base_url, link['href'])
-                for link in soup.find_all('a', href=True)
-                if base_url in urljoin(base_url, link['href'])]
+        urls = [
+            urljoin(base_url, link["href"])
+            for link in soup.find_all("a", href=True)
+            if base_url in urljoin(base_url, link["href"])
+        ]
 
-    url_data = UrlData(id=str(records_len), h1=titles.get('h1'), h2=titles.get('h2'), h3=titles.get('h3'), 
-                       h4=titles.get('h4'), h5=titles.get('h5'), paragraf_content=paragraphs, url=url)
+    url_data = UrlData(
+        id=str(records_len),
+        h1=titles.get("h1"),
+        h2=titles.get("h2"),
+        h3=titles.get("h3"),
+        h4=titles.get("h4"),
+        h5=titles.get("h5"),
+        paragraf_content=paragraphs,
+        url=url,
+    )
     return url_data, urls
 
+async def log_processed_records(records):
+    while True:
+        logger.warning(f'Processed {len(records)} records so far.')
+        await asyncio.sleep(10) 
 
 
-async def scrape_website_async(base_url):
-    """
-    Asynchronous web scraping function.
+async def scrape_website_async(base_url, concurrent_tasks):
+    session = aiohttp.ClientSession()
+    records = []
+    urls_to_parse = [(base_url, 0)]
+    tasks = set()
+    sem = asyncio.Semaphore(concurrent_tasks)  # Limit the number of concurrent tasks
 
-    Parameters:
-    base_url (str): The base URL to scrape data from.
+    robots = await fetch_robots(session, base_url)
 
-    Returns:
-    List[UrlData]: A list of UrlData objects, each containing data scraped from a specific URL.
-    """
-    async with aiohttp.ClientSession() as session:
-        records = []
-        urls_to_parse = [(base_url, 0)]
+    log_task = asyncio.create_task(log_processed_records(records))
 
-        robots = await fetch_robots(session, base_url)
+    try:
+        while urls_to_parse or tasks:  # Wait for tasks to complete
+            if urls_to_parse and len(tasks) < concurrent_tasks:
+                url, depth = urls_to_parse.pop(0)
 
-        while urls_to_parse:
-            url, depth = urls_to_parse.pop(0)
+                if not robots.can_fetch('*', url):
+                    logger.warning(f'URL disallowed by robots.txt: {url}')
+                    continue
 
-            if not robots.can_fetch('*', url):
-                logger.warning(f'URL disallowed by robots.txt: {url}')
-                continue
+                async with sem:
+                    task = asyncio.create_task(parse(session, base_url, url, depth, len(records)))
+                    tasks.add(task)
+            else:
+                done_tasks, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+                for task in done_tasks:
+                    url_data, new_urls = task.result()
+                    if url_data:
+                        records.append(url_data)
 
-            url_data, new_urls = await parse(session, base_url, url, depth, len(records))
-            if url_data:
-                records.append(url_data)
+                    urls_to_parse.extend((new_url, depth+1) for new_url in new_urls
+                                         if new_url not in (url for url, _ in urls_to_parse))
+    finally:
+        await session.close()
+        for task in tasks:
+            task.cancel()
+        log_task.cancel()
+        await asyncio.gather(*tasks, log_task, return_exceptions=True)
 
-            logging.debug(f'Processed {len(records)} records so far.')
+    return records
 
-            urls_to_parse.extend((new_url, depth+1) for new_url in new_urls
-                                 if new_url not in (url for url, _ in urls_to_parse))
-
-        return records
-
-
+async def main():
+    base_url = "www.mluvii.com"
+    for concurrent_tasks in [1, 10, 100]:
+        start = time.time()
+        await scrape_website_async(f"https://{base_url}/", concurrent_tasks)
+        end = time.time()
+        print(
+            f"Time taken with {concurrent_tasks} concurrent tasks: {end - start} seconds"
+        )
 
 if __name__ == "__main__":
     pass
+    #asyncio.run(main())
+    
